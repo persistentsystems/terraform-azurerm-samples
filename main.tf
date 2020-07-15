0f4369645889bbf9a13c425297de06d2b46d5000
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-14 23:04:33 -0400

    resoure group sample

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+
+provider "azurerm" {
+  version = "=2.18.0"
+  features {}
+}
+
+provider "random" {
+  version = "2.2.0"
+}
+
+provider "azuread" {
+  version = "=0.7.0"
+}
+
+module "active_sample" {
+  
+  source           = "./samples/log-analytics"
+
+}
---
+++ samples/log-analytics/main.tf
@@ -0,0 +1,19 @@
+
+locals {
+  context = {
+    application_name = "pstf"
+    environment_name = "dev"
+    resource_group_name = "pstf-dev"
+    location = "East US"
+    location_suffix = "us-east"
+  }
+}
+
+
+module "rg" {
+  
+  source           = "github.com/persistentsystems/terraform-azurerm/services/resource-group/base/v1"
+
+  context = local.context
+
+}
