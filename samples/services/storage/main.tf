c7ed39bd864a71cda5e9e001f9b61cd8b1f98d27
cyrusbond <cyrusbond@pm.me>
2020-07-17 11:46:36 -0500

    storage sample complete w/cleanup

--- samples/services/storage/main.tf
+++ samples/services/storage/main.tf
@@ -6,12 +6,17 @@ locals {
     location_suffix = "us-east"
   }
 
-module "rg-storage" {
+module "rg" {
   source  = "github.com/persistentsystems/terraform-azurerm/services/resource-group/base/v1"
   context = local.context
-  name = "storage-foo"
 }
 
 module "storage" {
     source = "github.com/persistentsystems/terraform-azurerm/blob/master/services/storage/endpoint/base/v1"
+    context = module.rg.context
+    service_settings = {
+        name = "${local.context.application_name}-${local.context.environment_name}-sample"
+        tier = "standard"
+        type = "GRS"
+    }
 }
