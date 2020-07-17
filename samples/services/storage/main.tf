e642aa66dfbe8cada08725b9d2e7728a621fa853
cyrusbond <cyrusbond@pm.me>
2020-07-17 11:13:34 -0500

    cleanup and update to local context
    fixed storate ref.

--- samples/services/storage/main.tf
+++ samples/services/storage/main.tf
@@ -1,41 +1,17 @@
 locals {
   context = {
-    event_delivery_schema = null
-    topic_name            = null
-    labels                = null
-    filters               = null
-    eventhub_id           = null
-    service_bus_topic_id  = null
-    service_bus_queue_id  = null
-    included_event_types  = null
+    application_name = "pstf"
+    environment_name = "fn"
+    location = "East US"
+    location_suffix = "us-east"
   }
 
-module "storage" {
+module "rg-storage" {
   source  = "github.com/persistentsystems/terraform-azurerm/services/resource-group/base/v1"
-
   context = local.context
   name = "storage-foo"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                      = format("%s%ssa", lower(replace(var.name, "/[[:^alnum:]]/", "")), random_string.unique.result)
-  resource_group_name       = azurerm_resource_group.storage.name
-  location                  = azurerm_resource_group.storage.location
-  account_kind              = "StorageV2"
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  access_tier               = var.access_tier
-  enable_https_traffic_only = true
-
-  blob_properties {
-    delete_retention_policy {
-      days = var.soft_delete_retention
-    }
-  }
-
-  resource "azurerm_storage_container" "storage" {
-  count                 = length(var.containers)
-  name                  = var.containers[count.index].name
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = var.containers[count.index].access_type
+module "storage" {
+    source = "github.com/persistentsystems/terraform-azurerm/blob/master/services/storage/endpoint/base/v1"
 }
