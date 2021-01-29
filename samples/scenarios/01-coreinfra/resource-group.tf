ab33bf0c96a94fd70284642e4455048d46d18d59
Steve Dillon <stvdilln@yaponcha.com>
2021-01-29 11:37:21 -0500

    changes for V1 of the API.

--- samples/scenarios/01-coreinfra/resource-group.tf
+++ samples/scenarios/01-coreinfra/resource-group.tf
@@ -1,5 +1,13 @@
 module my_resource_group {
     source  = "../../../submodules/terraform-azurerm/services/resource-group/base/v1"
-    context = var.context 
-    name = "myapp-dev-eastus"
+    context = {
+        application_name = "myapp"
+        environment_name = "dev"
+        location = {
+            name = "East US"
+            suffix = "east-us"
+            number = 1
+        }
+    }
+    name = "myapp-dev-east-us"
 }
--- samples/scenarios/01-coreinfra/variables.tf
+++ samples/scenarios/01-coreinfra/variables.tf
@@ -1,12 +1,8 @@
-# This is very similar to the output context, this context is missing
-# the final resource group name, and is just used to create
-# the ressource group, after that we can use the output of the 
-# resource group module.
-variable context {
-    default = {
-        application_name = "myapp"
-        environment_name = "dev"
-        location         = "East US"
-        location_suffix  = "east-us"
-    }
+variable logs_short_term_days {
+    description = "Number of days to hold logs in short term storage (Log Analytics)"
+    default = 30 
+}
+variable logs_long_term_days {
+    description = "Number of days to hold logs in short term storage (Azure Storage)"
+    default = 90
 }
