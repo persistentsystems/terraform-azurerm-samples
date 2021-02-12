97904d77de4c25f9705707159a2ac318bf1a67ea
Steve Dillon <stvdilln@yaponcha.com>
2021-02-12 14:36:54 -0500

    To avoid resource clases make the random suffix part of the default resource name.

--- samples/scenarios/05-microservices/main.tf
+++ samples/scenarios/05-microservices/main.tf
@@ -2,7 +2,7 @@ module "coreinfra" {
     source = "../01-coreinfra"
 }
 locals {
-  default_resource_name = "${module.coreinfra.context.application_name}-${module.coreinfra.context.environment_name}-${module.coreinfra.context.location.suffix}"
+  default_resource_name = "${module.coreinfra.context.application_name}-${module.coreinfra.context.environment_name}-${module.coreinfra.context.location.suffix}-${module.coreinfra.deploy_suffix}"
   # This is a random string to help ensure that our resoures have unique names, using the same
   # random suffix for different resources is sometimes a bit simpler than creating more randoms.
   random_string             = module.coreinfra.deploy_suffix
